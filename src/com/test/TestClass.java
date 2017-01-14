8f35109c8f91f46729e7fa7ed44a92822b7f746e
hi-riyazkhan <riyazkhan.java@gmail.com>
2017-01-14 13:23:31 +0800

    Test

---
+++ src/com/test/TestClass.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by khan on 1/14/2017.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package example;/**
+ * Created by khan on 1/14/2017.
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
